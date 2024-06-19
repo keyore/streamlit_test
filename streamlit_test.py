747489ad3e9d51734a5e1f4cdf5215fe284c57ce
keyore <keyore79@gmail.com>
2024-06-19 21:38:02 +0900

    Update streamlit_test.py

--- streamlit_test.py
+++ streamlit_test.py
@@ -18,7 +18,7 @@ if user_id == "keyore" and user_password == '1234':
     image_files = ['welcom.jpg','Vermeer.png', 'Gogh.png', 'Munch.png', 'Klimt.jpg', 'ShinYoonbok.png']
     sel_index = sel_options.index(user_opt)
     img_file = image_files[sel_index]
-    img_local = Image.open(f"img/{img_file}")
+    img_local = Image.open(f"{img_file}")
     st.image(img_local, caption=user_opt)
 
 
